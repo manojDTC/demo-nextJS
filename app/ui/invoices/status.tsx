cced0eee22e282f69e4821599c33322a7883fee4
manojDTC <manoj.ds@dtcinfotech.com>
2024-06-14 12:11:46 +0530

    pushing to repo

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -11,6 +11,13 @@ export default function InvoiceStatus({ status }: { status: string }) {
           'bg-green-500 text-white': status === 'paid',
         },
       )}
+      // className={clsx(
+      //   'inline-flex items-center rounded-full px-2 py-1 text-xs',
+      //   {
+      //     'bg-gray-100 text-gray-500': status === 'pending',
+      //     'bg-green-500 text-white': status === 'paid',
+      //   },
+      // )}
     >
       {status === 'pending' ? (
         <>
